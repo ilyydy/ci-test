1be3f42c8b318def4ae60d28fb0482b1420eff7f
ilyydy <ilyydy@gmail.com>
2022-10-20 20:30:30 +0800

    go

--- dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.b = exports.a = void 0;
-const b_1 = require("./b");
-Object.defineProperty(exports, "b", { enumerable: true, get: function () { return b_1.b; } });
-exports.a = 1;

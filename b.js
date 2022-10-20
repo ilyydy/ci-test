49c56a62e099d2841851b8cd3e3dcf1dc655b451
ilyydy <ilyydy@gmail.com>
2022-10-20 16:14:03 +0000

    Deploying to dist from  @ 79322420693ad42b7c013ba1f10157ffd34ac252 🚀

---
+++ b.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.b = void 0;
+const c = 2;
+exports.b = 1;
---
+++ index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.b = exports.a = void 0;
+const b_1 = require("./b");
+Object.defineProperty(exports, "b", { enumerable: true, get: function () { return b_1.b; } });
+exports.a = 1;
